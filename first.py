b6c1175a8e21a3f7f95381a6e32749cab8b47569
Asanali <asan.saem@gmail.com>
2023-11-26 21:41:23 +0900

    ytes

--- first.py
+++ first.py
@@ -1,8 +1,2 @@
-print("hello")
-a = 10
-for i in range(20):
-  print("yes")
-c = 10
-b = c > a
-v = a
-print("hello")
+def cout(name:str, age :int) -> str:
+  print(name.capitalize + " " + age.__abs__)
