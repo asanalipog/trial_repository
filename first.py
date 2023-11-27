cb933d9f71668c9b451d0bb185a4c6fe517978e4
Asanali <asan.saem@gmail.com>
2023-11-27 22:19:31 +0900

    d

--- first.py
+++ first.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-
+from typing import Generic, TypeAlias, TypeVar
 
 @dataclass
 class User:
@@ -17,9 +17,26 @@ print(cout(name , age))
 print("yes")
 def get_tuple(lst: list[float | bool]) -> tuple[int]:
     return tuple(int(num) for num in lst)
-
+"""
+The multiply function takes any number of inputs of int and multiply it together.
+Comparison with multiply(a, b) it can take any number of inputs
+As, I understood we can use any names instead of *args -> such as *name, etc.
+"""
 def multiply(*args:int):
   z = 1
   for num in args:
     z = z * num
   return z
+
+"""
+the **kwargs let us to control the inputs we will give to the function, however it is may be unordered.
+"""
+def print_values(**kwargs):
+    for key, value in kwargs.items():
+        print("The value of {} is {}".format(key, value))
+print_values(my_name="Sammy", your_name="Casey",
+            a = "Hui", b= "chlen")
+@dataclass
+class Hui:
+  name:str
+  length:int
