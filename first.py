e18f5cc49b3eca25b966398ce1149a1dc0354140
Asanali <asan.saem@gmail.com>
2023-11-29 14:56:14 +0900

    Rizabek asked#

--- first.py
+++ first.py
@@ -1,6 +1,86 @@
-import requests
-import time
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import Command
+from aiogram.types import Message
 
+a = 10
+# Вместо BOT TOKEN HERE нужно вставить токен вашего бота,
+# полученный у @BotFather
+BOT_TOKEN = '6600080191:AAFUBCn1GvknB7pcTQ6uViBFTU3mBxWsvzU'
+
+# Создаем объекты бота и диспетчера
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+
+# Этот хэндлер будет срабатывать на команду "/start"
+async def process_start_command(message: Message):
+    await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
+
+
+# Этот хэндлер будет срабатывать на команду "/help"
+async def process_help_command(message: Message):
+    await message.answer(
+        'Напиши мне что-нибудь и в ответ '
+        'я пришлю тебе твое сообщение'
+    )
+
+
+# Этот хэндлер будет срабатывать на отправку боту фото
+async def send_photo_echo(message: Message):
+    await message.reply_photo(message.photo[0].file_id)
+
+async def send_stic(message):
+    await message.reply_sticker(message.sticker.file_id)
+
+# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
+# кроме команд "/start" и "/help"f
+async def rep_audio(message:Message):
+    await message.reply_audio(message.audio.file_id)
+    await message.answer("It's fine audio")
+async def send_echo(message: Message):
+    await message.reply(text=message.text)
+
+
+# Регистрируем хэндлеры
+dp.message.register(send_stic, F.sticker)
+dp.message.register(rep_audio, F.audio)
+dp.message.register(process_start_command, Command(commands='start'))
+dp.message.register(process_help_command, Command(commands='help'))
+dp.message.register(send_photo_echo, F.photo)
+dp.message.register(send_echo)
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
 
 API_URL = 'https://api.telegram.org/bot'
 API_CATS_URL = 'https://api.thecatapi.com/v1/images/search'
@@ -14,7 +94,7 @@ counter = 0
 cat_response: requests.Response
 cat_link: str
 
-timeout = 10
+timeout = 0
 updates: dict
 
 
@@ -29,11 +109,19 @@ while True:
     if updates['result']:
         for result in updates['result']:
             offset = result['update_id']
-            do_something()
+            chat_id = result['message']['from']['id']
+            chat_mes = result['message']['text']
+            if chat_mes == "/help":
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text=Пришли мне любое предложение?')
+            elif chat_mes == "/start":
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text=Я готов начать работу')
+            else:requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={chat_mes}')
+            print(chat_mes)
+            timeout = 50
     end_time = time.time()
     print(f'Время между запросами к Telegram Bot API: {end_time - start_time}')
 
-
+"""
 
 
 """Code to reply photos
